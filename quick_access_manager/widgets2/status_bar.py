dcef959c5eb729c4b65458f28b14f855b1dc3c2e
toma_omito <taro.yamada@example.com>
2025-11-28 10:45:41 +0900

    Add status bar

--- quick_access_manager/widgets2/status_bar.py
+++ quick_access_manager/widgets2/status_bar.py
@@ -11,6 +11,12 @@ class StatusBarWidget(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.icon_dir = os.path.join(os.path.dirname(__file__), "icon")
+
+        # Initialize tracking variables before UI setup
+        self.is_selected = False
+        self.is_alphalock = False
+        self.is_inherit_alpha = False
+
         self.init_ui()
         self.update_status()
 
@@ -28,20 +34,32 @@ class StatusBarWidget(QWidget):
         self.selection_info_label = QLabel()
         self.selection_info_label.setFixedSize(18, 18)
         self.selection_info_label.setScaledContents(True)
-
-        self.alphalock_info_label = QLabel()
-        self.alphalock_info_label.setFixedSize(18, 18)
-        self.alphalock_info_label.setScaledContents(True)
+        self.selection_info_label.setPixmap(
+            QPixmap(os.path.join(self.icon_dir, "selection_off.png"))
+        )
+        self.selection_info_label.setToolTip("Selection: Off")
 
         self.inherit_alpha_info_label = QLabel()
         self.inherit_alpha_info_label.setFixedSize(18, 18)
         self.inherit_alpha_info_label.setScaledContents(True)
+        self.inherit_alpha_info_label.setPixmap(
+            QPixmap(os.path.join(self.icon_dir, "inherit_alpha_off.png"))
+        )
+        self.inherit_alpha_info_label.setToolTip("Inherit Alpha: Off")
+
+        self.alphalock_info_label = QLabel()
+        self.alphalock_info_label.setFixedSize(18, 18)
+        self.alphalock_info_label.setScaledContents(True)
+        self.alphalock_info_label.setPixmap(
+            QPixmap(os.path.join(self.icon_dir, "alpha_lock_off.png"))
+        )
+        self.alphalock_info_label.setToolTip("Alpha Lock: Off")
 
         layout.addWidget(self.selection_info_label)
         layout.addWidget(self._create_separator())
-        layout.addWidget(self.alphalock_info_label)
-        layout.addWidget(self._create_separator())
         layout.addWidget(self.inherit_alpha_info_label)
+        layout.addWidget(self._create_separator())
+        layout.addWidget(self.alphalock_info_label)
         layout.addStretch()
 
         self.setLayout(layout)
@@ -62,36 +80,50 @@ class StatusBarWidget(QWidget):
         return separator
 
     def update_status(self):
-        # Update selection icon
+        # Update selection icon only if status changed
         selection_info = self.get_selection_status()
-        selection_icon = "selection_on.png" if selection_info else "selection_off.png"
-        selection_tooltip = "Selection: On" if selection_info else "Selection: Off"
-        self.selection_info_label.setToolTip(selection_tooltip)
-        self.selection_info_label.setPixmap(
-            QPixmap(os.path.join(self.icon_dir, selection_icon))
-        )
+        if selection_info != self.is_selected:
+            self.is_selected = selection_info
+            selection_icon = (
+                "selection_on.png" if selection_info else "selection_off.png"
+            )
+            selection_tooltip = "Selection: On" if selection_info else "Selection: Off"
+            self.selection_info_label.setToolTip(selection_tooltip)
+            self.selection_info_label.setPixmap(
+                QPixmap(os.path.join(self.icon_dir, selection_icon))
+            )
 
-        # Update alpha lock icon
+        # Update alpha lock icon only if status changed
         alphalock_info = self.get_alphalock_status()
-        alphalock_icon = "alpha_lock_on.png" if alphalock_info else "alpha_lock_off.png"
-        alphalock_tooltip = "Alpha Lock: On" if alphalock_info else "Alpha Lock: Off"
-        self.alphalock_info_label.setToolTip(alphalock_tooltip)
-        self.alphalock_info_label.setPixmap(
-            QPixmap(os.path.join(self.icon_dir, alphalock_icon))
-        )
+        if alphalock_info != self.is_alphalock:
+            self.is_alphalock = alphalock_info
+            alphalock_icon = (
+                "alpha_lock_on.png" if alphalock_info else "alpha_lock_off.png"
+            )
+            alphalock_tooltip = (
+                "Alpha Lock: On" if alphalock_info else "Alpha Lock: Off"
+            )
+            self.alphalock_info_label.setToolTip(alphalock_tooltip)
+            self.alphalock_info_label.setPixmap(
+                QPixmap(os.path.join(self.icon_dir, alphalock_icon))
+            )
 
-        # Update inherit alpha icon
+        # Update inherit alpha icon only if status changed
         inherit_alpha_info = self.get_inherit_alpha_status()
-        inherit_alpha_icon = (
-            "inherit_alpha_on.png" if inherit_alpha_info else "inherit_alpha_off.png"
-        )
-        inherit_alpha_tooltip = (
-            "Inherit Alpha: On" if inherit_alpha_info else "Inherit Alpha: Off"
-        )
-        self.inherit_alpha_info_label.setToolTip(inherit_alpha_tooltip)
-        self.inherit_alpha_info_label.setPixmap(
-            QPixmap(os.path.join(self.icon_dir, inherit_alpha_icon))
-        )
+        if inherit_alpha_info != self.is_inherit_alpha:
+            self.is_inherit_alpha = inherit_alpha_info
+            inherit_alpha_icon = (
+                "inherit_alpha_on.png"
+                if inherit_alpha_info
+                else "inherit_alpha_off.png"
+            )
+            inherit_alpha_tooltip = (
+                "Inherit Alpha: On" if inherit_alpha_info else "Inherit Alpha: Off"
+            )
+            self.inherit_alpha_info_label.setToolTip(inherit_alpha_tooltip)
+            self.inherit_alpha_info_label.setPixmap(
+                QPixmap(os.path.join(self.icon_dir, inherit_alpha_icon))
+            )
 
     def get_selection_status(self):
         doc = Krita.instance().activeDocument()
